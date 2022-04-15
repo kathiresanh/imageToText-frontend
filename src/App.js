86f0181301b3f1d5a3472f2bfd35a89d32d566c2
KATHIRESAN <90540139+kathiresanh@users.noreply.github.com>
2022-04-15 22:14:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,66 +10,49 @@ import { createWorker } from "tesseract.js";
 
 function App() {
 
-  const [ocr, setOcr] = useState("");
-  const [count,setcount] = useState(0)
-  const [file,setfile] = useState(null)
-  const [image,setimage] = useState(null)
-  const onFormSubmit =async (e)=>{
-    e.preventDefault()
-
-    const formdata = new  FormData();
-    formdata.append("photo",file)
-    const config = {
-      headers :{
-        'content-type' :'multipary/form-data'
-      }
-    }
  
-   await axios.post("https://imagetotextend.herokuapp.com/user/upload",formdata,config).then(function(response){
-      alert("sucessfully")
-    }).catch((error)=>{
-      alert("error")
-    })
-
-  }
-  
-
+  var data = []
+  const [ocr, setOcr] = useState("");
+  const [imageData, setImageData] = useState(null);
+  const [count,setcount]=useState(0)
   const worker = createWorker({
     logger: (m) => {
-      
-      setcount(m.progress)
+        setcount(m.progress)
     },
   });
   const convertImageToText = async () => {
-    if (!image) return;
+    if (!imageData) return;
     await worker.load();
     await worker.loadLanguage("eng");
     await worker.initialize("eng");
     const {
       data: { text },
-    } = await worker.recognize(image);
+    } = await worker.recognize(imageData);
     setOcr(text);
   };
 
-  let oninputchange = event =>{
-    const images = event.target.files[0];
-    setfile(event.target.files[0])
-       if(!file)return;
+  useEffect(() => {
+    convertImageToText();
+  }, [imageData]);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    if(!file)return;
     const reader = new FileReader();
     reader.onloadend = () => {
       const imageDataUri = reader.result;
       console.log({ imageDataUri });
-      setimage(imageDataUri);
+      setImageData(imageDataUri);
     };
-    reader.readAsDataURL(images);
-  } 
-
+    reader.readAsDataURL(file);
+  }
+   
+  let changeImage = (e)=>{
+      console.log(e.target)
+      setImageData(e.target)
+  }
+  let imagetype = typeof(imageData)
   
-  useEffect(() => {
-    convertImageToText();
-  }, [image]);
-
-
 
   return (
     <div className="container-fluid">
@@ -77,21 +60,31 @@ function App() {
         <div className='col-sm-12' id='card1'>
        <div className='card' >
          <div className='card-body'>
-         <form onSubmit={onFormSubmit}>
-           <h1>Upload the image and convert it into text</h1>
-           <input type="file" name='photo' onChange={oninputchange}/>
-           <button type='submit'>upload</button>
-         </form>
+       
+           <h2>Upload the image and convert it into text</h2>
+           <input 
+           type="file"
+           name='photo' 
+           onChange={handleImageChange}
+          accept="image/*"/>
+        
+       
          </div>
        </div>
         </div>
         <div className='col-sm-12'>
           <div className='row'>
-            <div className='col-sm-6' id='imagetab'>
-            <img src={image} id="photos"></img>
+            <div className='col-sm-6 p-5' id='imagetab'>
+     
+          {typeof(imageData)== imagetype  ?   <img  src={imageData} id="photos"></img>: null  }
+            
+             <h4>Click on the images  convert to text </h4>
+            <img  src={`./images/1.jpg`}  className="p-3" onClick={(e)=>{changeImage(e)}} id="photos"></img>
+            <img  src={`./images/2.jpg`} className="p-3"  onClick={(e)=>{changeImage(e)}}  id="photos"></img>
+            <img  src={`./images/3.png`}  className="p-3" onClick={(e)=>{changeImage(e)}}  id="photos"></img>
             </div>
             <div className='col-sm-6' id='resulttab'>
-              <h1>Progress : {count*100}</h1>
+              <h2 style={{color:"white"}}>Progress : {count*100}</h2>
             <textarea rows="15"  cols="100" style={{color:"yellow",backgroundColor:"black"}} className='form-control' value={ocr}></textarea>
             </div>
           </div>
